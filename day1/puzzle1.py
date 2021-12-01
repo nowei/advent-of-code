cc5ac3c2e6275fd69700e421f7d5d3b9196c39d7
Andrew Wei <nowei@octoml.ai>
2021-12-01 09:17:30 -0800

    day 1

---
+++ day1/puzzle1.py
@@ -0,0 +1,11 @@
+arr = []
+with open('input1.txt', 'r') as f:
+    for line in f:
+        arr.append(int(line))
+
+bigger_than_prev = 0
+for i in range(1, len(arr)):
+    if arr[i - 1] < arr[i]:
+        bigger_than_prev += 1
+
+print("{} measurements were larger than the previous measurement".format(bigger_than_prev))
---
+++ day1/puzzle2.py
@@ -0,0 +1,15 @@
+arr = []
+with open('input1.txt', 'r') as f:
+    for line in f:
+        arr.append(int(line))
+
+bigger_than_prev = 0
+curr = sum(arr[0:3])
+prev = curr
+for i in range(3, len(arr)):
+    curr = curr - arr[i - 3] + arr[i]
+    if curr > prev:
+        bigger_than_prev += 1
+    prev = curr
+
+print("{} measurements were larger than the previous measurement".format(bigger_than_prev))
