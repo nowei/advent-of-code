ee57c39f4036d2156598942d15389a3efe9bb590
Andrew Wei <andrewwei1999@gmail.com>
2021-12-02 00:30:23 -0800

    advent day 2

---
+++ day2/puzzle3.py
@@ -0,0 +1,13 @@
+x = 0
+y = 0
+with open('input3.txt', 'r') as f:
+    for line in f:
+        d, amt = line.split()
+        amt = int(amt)
+        if d == 'down':
+            y += amt
+        elif d == 'up':
+            y -= amt
+        else:
+            x += amt
+print(x*y)
