1d26e08539b7927f52842330089e881960a7804c
Andrew Wei <a.nowei42@gmail.com>
2021-12-09 21:20:05 -0800

    reading is hard

---
+++ day10/puzzle19.py
@@ -0,0 +1,32 @@
+sample = False
+file = 'sample10.txt' if sample else 'input10.txt'
+invalids = {")": 0, "]": 0, "}": 0, ">": 0}
+scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
+with open(file, 'r') as f:
+    for line in f:
+        s = []
+        broken = False
+        for c in line.strip():
+            if c in {"(","[","{","<"}:
+                s.append(c)
+            else:
+                if c == ")" and s[-1] != "(":
+                    broken = True
+                    break
+                if c == "}" and s[-1] != "{":
+                    broken = True
+                    break
+                if c == "]" and s[-1] != "[":
+                    broken = True
+                    break
+                if c == ">" and s[-1] != "<":
+                    broken = True
+                    break
+                
+                s.pop()
+        if broken:
+            invalids[c] += 1
+total = 0
+for k in invalids:
+    total += invalids[k] * scores[k]
+print(total)
---
+++ day10/puzzle20.py
@@ -0,0 +1,47 @@
+sample = True
+file = 'sample10.txt' if sample else 'input10.txt'
+valids = []
+with open(file, 'r') as f:
+    for line in f:
+        s = []
+        broken = False
+        for c in line.strip():
+            if c in {"(","[","{","<"}:
+                s.append(c)
+            else:
+                if c == ")" and s[-1] != "(":
+                    broken = True
+                    break
+                if c == "}" and s[-1] != "{":
+                    broken = True
+                    break
+                if c == "]" and s[-1] != "[":
+                    broken = True
+                    break
+                if c == ">" and s[-1] != "<":
+                    broken = True
+                    break
+                
+                s.pop()
+        if not broken:
+            valids.append(s)
+scores = []
+for s in valids:
+    curr = 0
+    for c in s[::-1]:
+        v = 0
+        if c == '(':
+            v = 1
+        elif c == '[':
+            v = 2
+        elif c == '{':
+            v = 3
+        elif c == '<':
+            v = 4
+
+        curr *= 5 
+        curr += v
+    scores.append(curr)
+
+scores = sorted(scores)
+print(scores[len(scores)//2])
