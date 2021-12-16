74c96eb31d553da8aa2aa61ca021866a753a9772
Andrew Wei <a.nowei42@gmail.com>
2021-12-16 02:46:28 -0800

    adding day 16, parsing bits is kinda sus

---
+++ day16/puzzle32.py
@@ -0,0 +1,79 @@
+sample = False
+file = "sample16-2.txt" if sample else "input16.txt"
+ans = []
+
+def parse(byte_str, ind):
+    version_number = int(byte_str[ind:ind+3], 2)
+    packet_type = int(byte_str[ind+3:ind+6], 2)
+    total = version_number
+    if packet_type == 4: # it's a literal
+        # check every 5 bits 
+        i = 0
+        tmp = ""
+        while byte_str[ind+6+i * 5] != '0':
+            tmp += byte_str[ind+6+i*5+1:ind+6+i*5+5]
+            i += 1
+        tmp += byte_str[ind+6+i*5+1:ind+6+i*5+5]
+        end_ind = ind+6+i*5+5
+        total = int(tmp, 2)
+    else: # it's an operator
+        length_type = int(byte_str[ind+6], 2)
+        vals = []
+        if length_type == 0:
+            bits_in_subpackets = int(byte_str[ind+7:ind+22], 2)
+            end_ind = ind+22+bits_in_subpackets
+            next_ind = ind+22
+            while next_ind < end_ind:
+                temp, next_ind = parse(byte_str, next_ind)
+                vals.append(temp)
+            assert(next_ind == end_ind)
+        else: #length_type == 0
+            num_subpackets = int(byte_str[ind+7:ind+18], 2)
+            next_ind = ind + 18
+            for i in range(num_subpackets):
+                temp, next_ind = parse(byte_str, next_ind)
+                vals.append(temp)
+            end_ind = next_ind
+        if packet_type == 0:
+            total = sum(vals)
+        elif packet_type == 1:
+            total = 1
+            for v in vals:
+                total *= v
+        elif packet_type == 2:
+            total = min(vals)
+        elif packet_type == 3:
+            total = max(vals)
+        elif packet_type == 5:
+            assert(len(vals) == 2)
+            total = 1 if vals[0] > vals[1] else 0
+        elif packet_type == 6:
+            assert(len(vals) == 2)
+            total = 1 if vals[0] < vals[1] else 0
+        else:
+            assert(len(vals) == 2)
+            total = 1 if vals[0] == vals[1] else 0
+    return total, end_ind
+
+with open(file, "r") as f:
+    for line in f:
+        byte_str = ""
+        for c in line.strip():
+            byte_str += bin(int(c, 16))[2:].zfill(4)
+        next_ind = 0
+        total = 0
+        while next_ind < len(byte_str):
+            if all([c == '0' for c in byte_str[next_ind:]]): break
+            temp, next_ind = parse(byte_str, next_ind)
+            total += temp
+        ans.append(total)
+print(ans)
+if sample:
+    assert(ans[0] == 3)
+    assert(ans[1] == 54)
+    assert(ans[2] == 7)
+    assert(ans[3] == 9)
+    assert(ans[4] == 1)
+    assert(ans[5] == 0)
+    assert(ans[6] == 0)
+    assert(ans[7] == 1)
--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def generate_files(days):
                 f.write('        pass\n')
 
 def main():
-    generate_files([15])
+    generate_files([16])
 
 if __name__ == '__main__':
     main()
