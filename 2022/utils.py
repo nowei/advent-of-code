b63e908dd649f87e18f0aa3a2901ee6d4a9fd9fa
Andrew Wei <a.nowei42@gmail.com>
2022-12-01 16:59:14 +0800

    setting up utils.py to write base rust src file

--- 2022/utils.py
+++ 2022/utils.py
@@ -1,6 +1,48 @@
-import os
 import subprocess
 import toml
+import requests
+
+days = [2]
+
+def file_contents(d):
+    string = """use std::env;
+use std::fs;
+
+static SAMPLE: bool = true;
+
+static FILENAME: &str = match SAMPLE {
+    true => "sample.txt",
+    false => "input.txt",
+};
+
+fn puzzleDAYN1(contents: String) {
+    let result = 0;
+    println!("The result is:\\n{}", result);
+    return result;
+}
+
+fn puzzleDAYN2(contents: String) {
+    let result = 0;
+    println!("The result is:\\n{}", result);
+    return result;
+}
+
+fn main() {
+    let contents = fs::read_to_string(FILENAME)
+        .expect("Should have been able to read the file");
+    let result1 = puzzleDAYN1(contents);
+    if SAMPLE {
+        assert_eq!(result1, 0);
+    }
+    let result2 = puzzleDAYN2(contents);
+    if SAMPLE {
+        assert_eq!(result2, 0);
+    }
+}
+"""
+    string = string.replace("DAYN1", str(d * 2))
+    string = string.replace("DAYN2", str(d * 2 + 1))
+    return string
 
 def generate_files(days):
     data = toml.load("Cargo.toml")
@@ -10,27 +52,23 @@ def generate_files(days):
         p = subprocess.run(["cargo", "new", "day{}".format(d), "--bin"])
 
         with open(day + "/" + "sample.txt", 'w') as f: pass
-        # TODO: Pull input from internet
-        with open(day + "/" + "input.txt", 'w') as f: pass
+        req = requests.get("https://adventofcode.com/2022/day/{}/input".format(d))
+        with open(day + "/" + "input.txt", 'w') as f:
+            f.write(req.contents)
 
         if p.returncode == 0:
-            # TODO: Init loading file code
             with open(day + "/" + 'src/main.rs', 'w') as f:
-                f.write("""
-fn main() {
-    println!("Hello, worldd!");
-}
-                """)
-            member = day + "/Cargo.toml"
-            if member not in seen:
-                data["workspace"]["members"].append(member)
+                f.write(file_contents(d))
+
+            if day not in seen:
+                data["workspace"]["members"].append(day)
     
     with open("Cargo.toml", "w") as toml_file:
         toml.dump(data, toml_file)
 
 
 def main():
-    generate_files([2])
+    generate_files(days)
 
 if __name__ == '__main__':
     main()
