fe9df15b7d8b4365d3c7b90b16a56abc04d2f59d
Andrew Wei <a.nowei42@gmail.com>
2022-12-01 16:38:22 +0800

    day1 soln: iterate and then vectors

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,3 +1,62 @@
+use std::env;
+use std::fs;
+
+static SAMPLE: bool = false;
+
+static FILENAME: &str = match SAMPLE {
+    true => "sample.txt",
+    false => "input.txt",
+};
+
+fn puzzle1(contents: String) {
+    let mut current = 0;
+    let mut best = 0;
+    for line in contents.lines() {
+        match line {
+            "" => {
+                if current > best {
+                    best = current;
+                }
+                current = 0;
+            }
+            value => {
+                current += value.parse::<i32>().expect("This should be an int");
+            }
+        }
+    }
+    if current > best {
+        best = current;
+    }
+    println!();
+    println!("Most Calories:\n{}", best);
+}
+
+fn puzzle2(contents: String) {
+    let mut current = 0;
+    let mut entries = Vec::new();
+    for line in contents.lines() {
+        match line {
+            "" => {
+                entries.push(current);
+                current = 0;
+            }
+            value => {
+                current += value.parse::<i32>().expect("This should be an int");
+            }
+        }
+    }
+    entries.push(current);
+    entries.sort();
+    let top3 = &entries[entries.len()-3..];
+    let total: i32 = top3.iter().sum();
+    println!("The 3 elves carrying the most Calories total:\n{:?}", total);
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    let contents = fs::read_to_string(FILENAME)
+        .expect("Should have been able to read the file");
+
+    puzzle1(contents);
+    puzzle2(contents);
 }
