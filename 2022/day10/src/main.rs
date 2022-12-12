bf0b68a22dabbf00717bfe9227de679b5442379c
Andrew Wei <a.nowei42@gmail.com>
2022-12-12 00:32:26 -0800

    day 10 init: going to sleep

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -1,10 +1,4 @@
-import subprocess
-import toml
-
-days = [10]
-
-def file_contents(d):
-    string = """use std::fs;
+use std::fs;
 
 static SAMPLE_INPUT_FILENAME: &str = "sample.txt";
 static INPUT_FILENAME: &str = "input.txt";
@@ -13,9 +7,7 @@ type InputType = Vec<(char, char)>;
 
 fn parse_input(input: String) -> InputType {
     let mut result = InputType::new();
-    for line in input.lines() {
-        
-    }
+    for line in input.lines() {}
     return result;
 }
 
@@ -33,54 +25,27 @@ fn main() {
     let contents_sample =
         fs::read_to_string(SAMPLE_INPUT_FILENAME).expect("Should have been able to read the file");
     let parsed_input_sample = parse_input(contents_sample);
+    let contents_sample_large =
+        fs::read_to_string("larger_sample.txt").expect("Should have been able to read the file");
+    let parsed_input_sample_large = parse_input(contents_sample_large);
     let contents_actual =
         fs::read_to_string(INPUT_FILENAME).expect("Should have been able to read the file");
     let parsed_input_actual = parse_input(contents_actual);
 
     let sample_result_1 = puzzle1(&parsed_input_sample);
     assert_eq!(sample_result_1, 0);
+    let sample_result_1_large = puzzle1(&parsed_input_sample_large);
+    assert_eq!(sample_result_1_large, 13140);
     let sample_result_2 = puzzle2(&parsed_input_sample);
     assert_eq!(sample_result_2, 0);
 
     let actual_result_1 = puzzle1(&parsed_input_actual);
     let actual_result_2 = puzzle2(&parsed_input_actual);
 
-    println!("Day DAYN - Puzzle 1");
+    println!("Day 10 - Puzzle 1");
     println!("The sample result is: {}", sample_result_1);
     println!("The result for the input is: {}", actual_result_1);
-    println!("Day DAYN - Puzzle 2");
+    println!("Day 10 - Puzzle 2");
     println!("The sample result is: {}", sample_result_2);
     println!("The result for the input is: {}", actual_result_2);
 }
-
-"""
-    string = string.replace("DAYN", str(d))
-    return string
-
-def generate_files(days):
-    data = toml.load("Cargo.toml")
-    seen = set(data["workspace"]["members"])
-    for d in days:
-        day = "day{}".format(d)
-        # Note, this crashes if it already exists, so we only hit the input endpoint once
-        p = subprocess.run(["cargo", "new", "day{}".format(d), "--bin"])
-
-        if p.returncode == 0:
-            with open(day + "/" + 'src/main.rs', 'w') as f:
-                f.write(file_contents(d))
-
-            if day not in seen:
-                data["workspace"]["members"].append(day)
-
-            with open(day + "/" + "sample.txt", 'w') as f: pass
-            with open(day + "/" + "input.txt", 'wb') as f: pass
-    
-    with open("Cargo.toml", "w") as toml_file:
-        toml.dump(data, toml_file)
-
-
-def main():
-    generate_files(days)
-
-if __name__ == '__main__':
-    main()
