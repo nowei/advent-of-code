2963318c1e459f1ac004f02d28a82e64f6f52115
Andrew Wei <a.nowei42@gmail.com>
2023-12-01 21:13:50 +0800

    update util script

--- curr/utils.py
+++ curr/utils.py
@@ -1,23 +1,59 @@
 import os
 
-def generate_files(days):
-    for d in days:
-        day = 'day{}'.format(d)
-        os.mkdir(day)
-        sample_fn = 'sample{}.txt'.format(d)
-        input_fn = 'input{}.txt'.format(d)
-        with open(day + "/" + sample_fn, 'w') as f: pass
-        with open(day + "/" + input_fn, 'w') as f: pass
-        for i in range(2):
-            with open(day + "/" + 'puzzle{}.py'.format(d * 2 - i), 'w') as f:
-                f.write('sample = True\n')
-                f.write('file = "{}" if sample else "{}"\n'.format(sample_fn, input_fn))
-                f.write('with open(file, "r") as f:\n')
-                f.write('    for line in f:\n')
-                f.write('        pass\n')
+base_text = """
+from typing import Any
+
+sample_file = "test/{day}.sample"
+input_file = "test/{day}.input"
+sample_expected = 0
+
+def parse_file_{day}(file_path) -> Any:
+    with open(file_path, "r") as f:
+        for line in f:
+            pass
+
+def solve_{day}(input: Any) -> Any:
+    pass
+
+def main_{day}():
+    print("Running script for day {day}")
+    sample = parse_file_{day}(sample_file)
+    input = parse_file_{day}(input_file)
+
+    sample_out = solve_{day}(sample)
+
+    if sample_out != sample_expected:
+        print("Sample results do not match")
+        print("Sample expected:")
+        print(sample_expected)
+        print("Sample result:")
+        print(sample_out)
+    else:
+        print("Sample matched")
+    
+    out = solve_{day}(input)
+    print("This is the result:")
+    print(out)
+
+
+if __name__ == "__main__":
+    main_{day}()
+"""
+
+def generate_files(day: int):
+    s = "{:02d}".format(day)
+    day_dict = {"day": s}
+    with open("src/day{day}.py".format_map(day_dict), "w") as f:
+        out = base_text.format_map(day_dict)
+        f.write(out)
+    # touch files to create them
+    with open("test/{day}.sample".format_map(day_dict), "w") as f:
+        pass
+    with open("test/{day}.input".format_map(day_dict), "w") as f:
+        pass
 
 def main():
-    generate_files([24])
+    generate_files(2)
 
 if __name__ == '__main__':
     main()
