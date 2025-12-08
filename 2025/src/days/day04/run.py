4909435306ce682623306141ff19f0cd8ca4d737
Andrew Wei <a.nowei42@gmail.com>
2025-12-07 21:36:02 -0800

    [nit] fix typing

--- 2025/src/days/day04/run.py
+++ 2025/src/days/day04/run.py
@@ -40,30 +40,30 @@ def part1(_input: InputType) -> int:
 def part2(_input: InputType) -> int:
     rolls_removed = True
     total_rolls_removed = 0
-    _input = [list(s) for s in _input]
+    new_input = [list(s) for s in _input]
     while rolls_removed:
         rolls_removed = False
         changing_rolls = []
-        for i in range(len(_input)):
-            for j in range(len(_input[0])):
-                if _input[i][j] != "@":
+        for i in range(len(new_input)):
+            for j in range(len(new_input[0])):
+                if new_input[i][j] != "@":
                     continue
                 num_rolls = 0
                 for d_i in range(-1, 2):
                     for d_j in range(-1, 2):
                         if d_i == 0 and d_j == 0:
                             continue
-                        if i + d_i < 0 or i + d_i >= len(_input):
+                        if i + d_i < 0 or i + d_i >= len(new_input):
                             continue
-                        if j + d_j < 0 or j + d_j >= len(_input[0]):
+                        if j + d_j < 0 or j + d_j >= len(new_input[0]):
                             continue
-                        if _input[i + d_i][j + d_j] == "@":
+                        if new_input[i + d_i][j + d_j] == "@":
                             num_rolls += 1
                 if num_rolls < 4:
                     changing_rolls.append((i, j))
         total_rolls_removed += len(changing_rolls)
         for i, j in changing_rolls:
-            _input[i][j] = "."
+            new_input[i][j] = "."
         if changing_rolls:
             rolls_removed = True
 
